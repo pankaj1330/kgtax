331c81bdac1977236b30a3d22f3f41a69624c030
pankaj1330 <113126705+pankaj1330@users.noreply.github.com>
2022-10-10 22:38:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,115 +12,99 @@ function hide() {
 const details = [
     {
         inside: "Registeration",
-        src: "",
-        title: "",
-        price: "&#8377 499",
-        disc: ""
+        title: "GST <br> Registeration",
+        price: "<sup>&#8377</sup>499",
+        disc: "Get your GST Registration done easily through KG Tax. Feel free to Call or Whatsapp us and Our Experts will follow up and ensure you to get your registration done at the earliest"
     },
     {
         inside: "Registeration",
-        src: "",
-        title: "",
-        price: "&#8377 499",
-        disc: ""
+        title: "TRADEMARK <br> Registeration",
+        price: "<sup>&#8377</sup>499",
+        disc: "Get your Trademark Registration done easily through KG Tax. Feel free to Call or Whatsapp us and Our Experts will follow up and ensure you to get your registration done at the earliest."
     },
     {
         inside: "Registeration",
-        src: "",
-        title: "",
-        price: "&#8377 499",
-        disc: ""
+        title: "IMPORT EXPORT CODE <br> Registeration",
+        price: "<sup>&#8377</sup>499",
+        disc: "Get your Import Export Code Registration done easily through KG Tax. Feel free to Call or Whatsapp us and Our Experts will follow up and ensure you to get your registration done at the earliest."
     },
     {
         inside: "Registeration",
-        src: "",
-        title: "",
-        price: "&#8377 499",
-        disc: ""
+        title: "AD CODE <br> Registeration",
+        price: "<sup>&#8377</sup>499",
+        disc: "Get your AD Code Registration done easily through KG Tax. Feel free to Call or Whatsapp us and Our Experts will follow up and ensure you to get your registration done at the earliest."
     },
     {
         inside: "dsc",
-        src: "",
-        title: "",
-        price: "&#8377 499",
-        disc: ""
+        title: "DSC Class  3 Signing -  (No Hidden Charges)",
+        price: "<sup>&#8377</sup>499",
+        disc: "Feel free to call or whatsapp us and Our Expert will follow up and ensure you to get your DSC earliest"
     },
     {
         inside: "dsc",
-        src: "",
-        title: "",
-        price: "&#8377 499",
-        disc: ""
+        title: "DSC Class  3 Signing with Token (No Hidden Charges)",
+        price: "<sup>&#8377</sup>699",
+        disc: "Feel free to call or whatsapp us and Our Expert will follow up and ensure you to get your DSC earliest"
     },
     {
         inside: "dsc",
-        src: "",
-        title: "",
-        price: "&#8377 499",
-        disc: ""
+        title: "DSC Class  3 Combo (Sign+Encryption) with USB Token (No Hidden Charges)",
+        price: "<sup>&#8377</sup>1299",
+        disc: "Feel free to call or whatsapp us and Our Expert will follow up and ensure you to get your DSC earliest"
     },
     {
         inside: "dsc",
-        src: "",
-        title: "",
-        price: "&#8377 499",
-        disc: ""
+        title: "DSC for E-TENDER with USB Token (No Hidden Charges)",
+        price: "<sup>&#8377</sup>1299",
+        disc: "Feel free to call or whatsapp us and Our Expert will follow up and ensure you to get your DSC earliest"
     },
     {
         inside: "income tax",
-        src: "",
-        title: "",
-        price: "&#8377 499",
-        disc: ""
+        title: "Income Tax Return - Salaried / Pension Income",
+        price: "<sup>&#8377</sup>499",
+        disc: "Feel free to Call or Whatsapp us and Our Experts will follow up and ensure you to get your Income Tax return filed at the earliest"
     },
     {
         inside: "income tax",
-        src: "",
-        title: "",
-        price: "&#8377 499",
-        disc: ""
+        title: "Income Tax Return Business 44AD",
+        price: "<sup>&#8377</sup>999",
+        disc: "Feel free to Call or Whatsapp us and Our Experts will follow up and ensure you to get your Income Tax return filed at the earliest"
     },
     {
         inside: "income tax",
-        src: "",
-        title: "",
-        price: "&#8377 499",
-        disc: ""
+        title: "Income Tax Return Professionals  44ADA",
+        price: "<sup>&#8377</sup>999",
+        disc: "Feel free to Call or Whatsapp us and Our Experts will follow up and ensure you to get your Income Tax return filed at the earliest"
     },
     {
         inside: "income tax",
-        src: "",
-        title: "",
-        price: "&#8377 499",
-        disc: ""
+        title: "Income Tax Return   TRUSTS",
+        price: "<sup>&#8377</sup>2999",
+        disc: "Feel free to Call or Whatsapp us and Our Experts will follow up and ensure you to get your Income Tax return filed at the earliest"
     },
     {
         inside: "services tax",
-        src: "",
-        title: "",
-        price: "&#8377 499",
-        disc: ""
+        title: "GST registrations",
+        price: "<sup>&#8377</sup>499",
+        disc: "Feel free to Call or Whatsapp us and Our Experts will follow up and ensure you to get your Income Tax return filed at the earliest"
     },
     {
         inside: "services tax",
-        src: "",
-        title: "",
-        price: "&#8377 499",
-        disc: ""
+        title: "Gst Returns",
+        price: "<sup>&#8377</sup>499",
+        disc: "Feel free to Call or Whatsapp us and Our Experts will follow up and ensure you to get your Income Tax return filed at the earliest"
     },
     {
         inside: "services tax",
-        src: "",
-        title: "",
-        price: "&#8377 499",
-        disc: ""
+        title: "GST Annual Return GSTR-9",
+        price: "<sup>&#8377</sup>2499",
+        disc: "Feel free to Call or Whatsapp us and Our Experts will follow up and ensure you to get your Income Tax return filed at the earliest"
     },
     {
         inside: "services tax",
-        src: "",
-        title: "",
-        price: "&#8377 499",
-        disc: ""
+        title: "GSTR 10 Final Return",
+        price: "<sup>&#8377</sup>1299",
+        disc: "Feel free to Call or Whatsapp us and Our Experts will follow up and ensure you to get your Income Tax return filed at the earliest"
     },
 ];
 
@@ -130,8 +114,7 @@ window.addEventListener('DOMContentLoaded', () => {
     let displayDetails = details.map(function (item) {
         if (item.inside == "Registeration") {
             return `<div class="detail" id="detail4">
-        <img src=${item.src} alt="">
-         <h2 class="title">${item.inside}</h2>
+         <h2 class="title">${item.title}</h2>
          <h1 class="price">${item.price}</h1>
          <p class="disc">${item.disc}</p>
         </div>`;
@@ -144,8 +127,7 @@ function registeration(){
     let displayDetails = details.map(function (item) {
         if (item.inside == "Registeration") {
             return `<div class="detail" id="detail4">
-        <img src=${item.src} alt="">
-         <h2 class="title">${item.inside}</h2>
+         <h2 class="title">${item.title}</h2>
          <h1 class="price">${item.price}</h1>
          <p class="disc">${item.disc}</p>
         </div>`;
@@ -158,8 +140,7 @@ function dsc(){
     let displayDetails = details.map(function (item) {
         if (item.inside == "dsc") {
             return `<div class="detail" id="detail4">
-        <img src=${item.src} alt="">
-         <h2 class="title">${item.inside}</h2>
+         <h2 class="title">${item.title}</h2>
          <h1 class="price">${item.price}</h1>
          <p class="disc">${item.disc}</p>
         </div>`;
@@ -172,8 +153,7 @@ function incometax(){
     let displayDetails = details.map(function (item) {
         if (item.inside == "income tax") {
             return `<div class="detail" id="detail4">
-        <img src=${item.src} alt="">
-         <h2 class="title">${item.inside}</h2>
+         <h2 class="title">${item.title}</h2>
          <h1 class="price">${item.price}</h1>
          <p class="disc">${item.disc}</p>
         </div>`;
@@ -186,8 +166,7 @@ function servicetax(){
     let displayDetails = details.map(function (item) {
         if (item.inside == "services tax") {
             return `<div class="detail" id="detail4">
-        <img src=${item.src} alt="">
-         <h2 class="title">${item.inside}</h2>
+         <h2 class="title">${item.title}</h2>
          <h1 class="price">${item.price}</h1>
          <p class="disc">${item.disc}</p>
         </div>`;
